30a340aa9fc1c5dd42ece101ea3809c83606cc9c
Muifrend <andrzejzadaniadomowe1@gmail.com>
2025-10-16 20:04:02 -0700

    1999D_revised

--- 1999D_Slavic's_Exam.py
+++ 1999D_Slavic's_Exam.py
@@ -1,18 +1,18 @@
 for _ in range(int(input())):
-    f = input()
-    s = input()
+    f = list(input())
+    s = "".join(reversed(input()))
     counter = 0
     for i in range(len(f)-1,-1,-1):
         if counter == len(s):
             break
-        if f[i]==s[counter]:
+        if f[i] == '?':
+            f[i] = s[counter]
             counter+=1
-        elif f[i] == '?':
-            f = f.replace('?', s[counter], 1)
+        elif f[i]==s[counter]:
             counter+=1
-
+    ff = "".join(f)
     if counter == len(s):
         print('Yes')
-        print(f.replace('?','a'))
+        print(ff.replace('?','a'))
     else:
         print('No')
